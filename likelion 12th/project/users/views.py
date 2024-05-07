fdd2336b7d3c31aaf2e9b47c677001a4e3169eb8
onlynyang <chica1875@gmail.com>
2024-05-07 22:37:13 +0900

    Fix : mypage목록 수정

--- likelion 12th/project/users/views.py
+++ likelion 12th/project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
-from .models import Post
+from main.models import Post
 
 def mypage(request):
     posts = Post.objects.all()
