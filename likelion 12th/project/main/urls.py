dfa17dc9d665626cbbfc65dbf0ae5a27a25e993a
onlynyang <chica1875@gmail.com>
2024-05-17 01:15:13 +0900

    Feat : 4주차 과제

--- likelion 12th/project/main/urls.py
+++ likelion 12th/project/main/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('edit/<int:id>', edit, name="edit"),
     path('update/<int:id>', update, name="update"),
     path('delete/<int:id>', delete, name="delete"),
+    path('delete-comment/<int:id>', delete_comment, name="delete_comment"),
+    path('tag-list', tag_list, name="tag-list"),
+    path('tag-posts/<int:tag_id>', tag_posts, name="tag-posts"),
 ] 
--- likelion 12th/project/main/views.py
+++ likelion 12th/project/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
-from .models import Post
+from .models import Post, Comment, Tag
 
 # Create your views here.
 
@@ -19,38 +19,89 @@ def new_post(request):
     return render(request, 'main/new-post.html')
 def detail(request, id):
     post = get_object_or_404(Post, pk = id)
-    return render(request, 'main/detail.html', {'post':post})
+    if request.method == 'GET':
+        comments = Comment.objects.filter(post=post)
+        return render(request, 'main/detail.html', {'post':post, 'comments':comments})
+
+    elif request.method == 'POST':
+        new_comment = Comment()
+
+        new_comment.post = post
+        new_comment.writer = request.user
+        new_comment.content = request.POST['content']
+        new_comment.pub_date = timezone.now()
+
+        new_comment.save()
+        return redirect('main:detail', id)
+
 def edit(request, id):
     edit_post = Post.objects.get(pk=id)
     return render(request, 'main/edit.html', {'post' : edit_post})
 def create(request):
-    new_post = Post()
+    if request.user.is_authenticated:
+        new_post = Post()
 
-    new_post.title = request.POST['title']
-    new_post.category = request.POST['category']
-    new_post.writer = request.POST['writer']
-    new_post.body = request.POST['body']
-    new_post.pub_date = timezone.now()
-    new_post.image = request.FILES.get('image')
+        new_post.title = request.POST['title']
+        new_post.category = request.POST['category']
+        new_post.writer = request.user
+        new_post.body = request.POST['body']
+        new_post.pub_date = timezone.now()
+        new_post.image = request.FILES.get('image')
 
-    new_post.save()
+        new_post.save()
 
-    return redirect('main:detail', new_post.id)
+        words = new_post.body.split(' ')
+        tag_list = []
+
+        for w in words:
+            if len(w)>0:
+                if w[0] == '#':
+                    tag_list.append(w[1:])
+
+        for t in tag_list:
+            tag, boolean = Tag.objects.get_or_create(name=t)
+            new_post.tags.add(tag.id)
+
+        return redirect('main:detail', new_post.id)
+
+    else:
+        return redirect('accounts:login')
 
 def update(request, id):
     update_post = Post.objects.get(pk=id)
+    if request.user.is_authenticated and request.user == update_post.writer:
+        update_post.title = request.POST['title']
+        update_post.category = request.POST['category']
+        update_post.body = request.POST['body']
+        update_post.pub_date = timezone.now()
+        
+        if request.FILES.get('image'):
+            update_post.image = request.FILES.get['image']
 
-    update_post.title = request.POST['title']
-    update_post.category = request.POST['category']
-    update_post.writer = request.POST['writer']
-    update_post.body = request.POST['body']
-    update_post.pub_date = timezone.now()
-    update_post.image = request.FILES.get('image')
+        update_post.save()
+        return redirect('main:detail', update_post.id)
+    return redirect('accounts:login', update_post.id)
 
-    update_post.save()
 
-    return redirect('main:detail', update_post.id)
 def delete(request, id):
     delete_post = Post.objects.get(pk=id)
     delete_post.delete()
     return redirect('main:secondpage')
+
+def delete_comment(request, id):
+    comment = Comment.objects.get(pk=id)
+    if request.user == comment.writer:
+        comment.delete()
+    return redirect('main:detail', id=comment.post.id)
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'main/tag-list.html', { 'tags' : tags })
+
+def tag_posts(request, tag_id):
+    tag = get_object_or_404(Tag, id=tag_id)
+    posts = tag.posts.all()
+    return render(request, 'main/tag-post.html', {
+        'tag' : tag,
+        'posts' : posts
+    })
