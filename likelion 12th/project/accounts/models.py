52e665b53db4e205e4d37ac1e5f8b014e674819d
onlynyang <chica1875@gmail.com>
2024-07-12 21:36:16 +0900

    Fix: 회원가입 버그 수정

--- likelion 12th/project/accounts/models.py
+++ likelion 12th/project/accounts/models.py
@@ -1,9 +1,9 @@
+# accounts/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.TextField(max_length=10)
@@ -15,9 +15,10 @@ class Profile(models.Model):
         return self.user.username
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, **kwarags):
-    instance.profile.save()
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwarags):
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
--- likelion 12th/project/accounts/views.py
+++ likelion 12th/project/accounts/views.py
@@ -1,8 +1,8 @@
+# accounts/views.py
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth.models import User
 from .models import Profile
-# Create your views here.
 
 def login(request):
     if request.method == 'POST':
@@ -10,7 +10,6 @@ def login(request):
         password = request.POST['password']
 
         user = auth.authenticate(request, username=username, password=password)
-
 
         if user is not None:
             auth.login(request, user)
@@ -27,12 +26,14 @@ def logout(request):
 
 def signup(request):
     if request.method == 'POST':
-
         if request.POST['password'] == request.POST['confirm']:
             user = User.objects.create_user(
-                username = request.POST['username'],
-                password = request.POST['password']
+                username=request.POST['username'],
+                password=request.POST['password']
             )
+
+            # Profile 객체가 없을 경우 이를 생성
+            Profile.objects.get_or_create(user=user)
 
             user.profile.nickname = request.POST['nickname']
             user.profile.department = request.POST['department']
